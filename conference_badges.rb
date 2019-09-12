f3b6de205bdbb6cafc87b34e772125e32cc7d23f
Emily Harber <emily.a.harber@gmail.com>
2019-09-12 21:44:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,8 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}"
+end 
+
 def batch_badge_creator(speakers)
   badges = []
   speakers.each{ |name| badges << "Hello, my name is #{name}." }
